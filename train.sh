d2c20bbc9a43d640eacca62b12fb247164559025
Kotomiya07 <115519148+Kotomiya07@users.noreply.github.com>
2025-10-10 23:24:19 +0900

    add uv run command

--- train.sh
+++ train.sh
@@ -3,4 +3,4 @@ GPU=$1
 CFG=$2
 TAG=${3:-'default'}
 
-torchrun --nproc_per_node $GPU --master_port $PORT main.py --cfg $CFG --data-path <path-to-imagenet> --amp --tag $TAG
+uv run torchrun --nproc_per_node $GPU --master_port $PORT main.py --cfg $CFG --amp --wandb
